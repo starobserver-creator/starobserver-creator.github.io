01a8a669c5c6df95788b8199b01dc455d0bbab7a
starkobserver <48153992+starobserver-creator@users.noreply.github.com>
2026-03-29 14:10:38 -0700

    fixed default behavior to use built in behaviors

--- fancifymytext.js
+++ fancifymytext.js
@@ -2,16 +2,36 @@ const BigButton = document.getElementById("Bigger");
 
 BigButton.addEventListener("click", function() {
     // alert("Hello, world!");
-    document.getElementById("TextArea").style.fontSize = "24pt";
+    TextArea.style.fontSize = "24pt";
 });
 
 document.getElementById("fancy").addEventListener("change",function(){
-    document.getElementById("TextArea").style.fontFamily = "cursive";
-    document.getElementById("boring").checked = false;
+    if(this.checked){
+        alert("Fancifying the text!");
+        TextArea.style.color = "blue";
+        TextArea.style.fontWeight = "bold";
+        TextArea.style.textDecoration = "underline";
 
+        
+    }
+   
+
+});
+document.getElementById("Moo").addEventListener("click",function(){
+    const text = TextArea.value.toUpperCase();
+    let parts = text.split(".");
+    TextArea.value = parts.join("-Moo.");
+    
+    
 });
 
 document.getElementById("boring").addEventListener("change",function(){
-    document.getElementById("TextArea").style.fontFamily = "Arial";
-    document.getElementById("fancy").checked = false;
+        if(this.checked){
+            alert("Back to boring...");
+            TextArea.style.color = "black";
+            TextArea.style.fontWeight = "normal";
+            TextArea.style.fontWeight = "normal";
+            TextArea.style.textDecoration = "none";
+            
+        }
 });
