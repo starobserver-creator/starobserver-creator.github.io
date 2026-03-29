ed1d76a7ee94a5492d688b6f6c429e1fc6218f36
starkobserver <48153992+starobserver-creator@users.noreply.github.com>
2026-03-29 13:33:57 -0700

    fixed js probelm

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,1 +1,17 @@
-alert("Hello, world!");
+const BigButton = document.getElementById("Bigger");
+
+BigButton.addEventListener("click", function() {
+    // alert("Hello, world!");
+    document.getElementById("TextArea").style.fontSize = "24pt";
+});
+
+document.getElementById("fancy").addEventListener("change",function(){
+    document.getElementById("TextArea").style.fontFamily = "cursive";
+    document.getElementById("boring").checked = false;
+
+});
+
+document.getElementById("boring").addEventListener("change",function(){
+    document.getElementById("TextArea").style.fontFamily = "Arial";
+    document.getElementById("fancy").checked = false;
+});
